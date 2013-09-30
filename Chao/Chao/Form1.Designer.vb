85f919acc7ed1b40534a8acfbe6dafcc165dc976
cbtzeng <cbtzeng@gmail.com>
2013-10-01 02:29:35 +0800

    choose machines, input L for s1 s2 s3

--- Chao/Chao/Form1.Designer.vb
+++ Chao/Chao/Form1.Designer.vb
@@ -22,9 +22,132 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"開挖機", "拖索開挖機", "抓斗開挖機", "履帶起重機", "推土機", "牽引裝料機", "振動式打樁機", "鑽岩機", "壓路機", "輪胎式壓路機", "振動式壓路機", "瀝青混凝土舖料機(延長部分除外)", "混凝土割切機", "發電機(不含輪胎)", "空氣壓縮機(不含輪胎)", "卡車起重機", "輪形起重機", "混凝土泵", "油壓式打樁機", "油壓式鋼管壓入機", "拔樁機", "油壓式壓入拉拔樁機", "土壤取樣器", "全套管鑽掘機", "鑽土機", "混凝土破碎機"})
+        Me.ComboBox1.Location = New System.Drawing.Point(20, 38)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(287, 20)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "請選擇"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(15, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 26)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "機具選擇"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(20, 101)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(387, 260)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("微軟正黑體", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(22, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 21)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "機具圖示"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label3.Location = New System.Drawing.Point(424, 53)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 26)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "輸入L :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label4.Location = New System.Drawing.Point(469, 100)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(30, 26)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "r :"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(517, 57)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(99, 22)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(517, 104)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(96, 22)
+        Me.TextBox2.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(631, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(42, 21)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "確認"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(721, 502)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
--- Chao/Chao/Form1.vb
+++ Chao/Chao/Form1.vb
@@ -1,3 +1,63 @@
-﻿Public Class Form1
+﻿
 
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub DomainUpDown1_SelectedItemChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.Text = "開挖機" Then
+            PictureBox1.Image = My.Resources.Resource1.小型開挖機_compact_excavator_
+        End If
+        If ComboBox1.Text = "拖索開挖機" Then
+            PictureBox1.Image = My.Resources.Resource1.小型膠輪式裝料機_compact_loader__wheeled_
+        End If
+        If ComboBox1.Text = "抓斗開挖機" Then
+            PictureBox1.Image = My.Resources.Resource1.滑移裝料機_skid_steer_loader_
+        End If
+        If ComboBox1.Text = "履帶起重機" Then
+            PictureBox1.Image = My.Resources.Resource1.履帶式開挖裝料機_crawler_backhoe_loader_
+        End If
+        If ComboBox1.Text = "推土機" Then
+            PictureBox1.Image = My.Resources.Resource1.履帶式推土機_crawler_dozer_
+        End If
+        If ComboBox1.Text = "牽引裝料機" Then
+            PictureBox1.Image = My.Resources.Resource1.履帶式裝料機_crawler_loader_
+        End If
+        If ComboBox1.Text = "振動式打樁機" Then
+            PictureBox1.Image = My.Resources.Resource1.履帶式開挖機_crawler_excavator_
+        End If
+        If ComboBox1.Text = "鑽岩機" Then
+            PictureBox1.Image = My.Resources.Resource1.膠輪式推土機_wheeled_dozer_
+        End If
+        If ComboBox1.Text = "壓路機" Then
+            PictureBox1.Image = My.Resources.Resource1.膠輪式開挖裝料機_wheeled_backhoe_loader_
+        End If
+        If ComboBox1.Text = "輪胎式壓路機" Then
+            PictureBox1.Image = My.Resources.Resource1.膠輪式開挖機_wheeled_excavator_
+        End If
+        If ComboBox1.Text = "振動式壓路機" Then
+            PictureBox1.Image = My.Resources.Resource1.膠輪式裝料機_wheeled_loader_
+        End If
+        If ComboBox1.Text = "混凝土割切機" Then
+            PictureBox1.Image = My.Resources.Resource1.壓路機_rollers_
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text < 1.5 Then
+            TextBox2.Text = "4"
+        End If
+        If TextBox1.Text >= 1.5 And TextBox1.Text < 4 Then
+            TextBox2.Text = "10"
+        End If
+        If TextBox1.Text >= 4 Then
+            TextBox2.Text = "16"
+        End If
+    End Sub
 End Class
