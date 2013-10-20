9348b72fc4c73d6ba321dbe1e5e50687b62db44e
timera <timerakkle@gmail.com>
2013-10-21 00:30:47 +0800

    adding functionality that enables graphing with settable time

--- Chao/Chao/Graphing.vb
+++ Chao/Chao/Graphing.vb
@@ -1,14 +1,16 @@
 ﻿Imports Microsoft.VisualBasic
 Imports System.Windows.Forms.DataVisualization.Charting
 
-
 Public Class CGraph
-    'private global variables
+    'protected global variables
     Protected chart As Chart
     Protected series As System.Collections.Generic.List(Of Series)
     Protected legend As Legend
     Protected TestMode As Modes
     Protected NumOfMics As Integer
+    Protected Panel As Panel
+    Protected SecTextFontSize As Integer = 12
+    Protected SecTextBoxSize As Size = New Size(50, 20)
 
     Enum Modes
         A1A2A3
@@ -16,12 +18,6 @@ Public Class CGraph
     End Enum
 
     Public Sub New()
-        chart = New Chart
-        chart.Location = New Point(0, 0)
-        chart.Size = New Size(100, 100)
-        chart.ChartAreas.Add(New ChartArea("ChartArea1"))
-        chart.Legends.Add(New Legend("Legend1"))
-        series = New List(Of Series)
     End Sub
     'Constructor
     'loc = Location
@@ -37,12 +33,23 @@ Public Class CGraph
                    )
         'Plot controls
         chart = New Chart
-        chart.Location = loc
-        chart.Size = size
-        parent.Controls.Add(chart)
+        Panel = New Panel()
+        parent.Controls.Add(Panel)
+        Panel.Controls.Add(chart)
+        'Panel.Name = "Panel"
+        Panel.Size = size
+        Panel.Location = loc
+
+
+        chart.Location = New Point(-20, -5) 'so it hugs the border
+        chart.Size = size - New Size(0, SecTextBoxSize.Height)
         chart.ChartAreas.Add(New ChartArea("ChartArea1"))
         chart.Legends.Add(New Legend("Legend1"))
+
         series = New List(Of Series)
+
+
+
         TestMode = mode
         If mode = Modes.A1A2A3 Then
             NumOfMics = 6
@@ -57,6 +64,7 @@ Public Class CGraph
             s.IsValueShownAsLabel = False
             chart.Series.Add(s)
         Next
+
     End Sub
     'Update function
     Public Overridable Sub Update(ByVal newVal() As Integer)
@@ -66,15 +74,49 @@ End Class
 
 Public Class LineGraph
     Inherits CGraph
+    'private global vars
+    Dim SecTextBox As MaskedTextBox
+    Dim TimeInSec As Integer
 
     'constructor
     Public Sub New(ByVal loc As Point,
                    ByVal size As Size,
                    ByVal parent As Control,
-                   ByVal mode As Modes
+                   ByVal mode As Modes,
+                   ByVal time As Integer
                    )
         MyBase.New(loc, size, parent, SeriesChartType.Line, mode)
+        SecTextBox = New MaskedTextBox()
+        Panel.Controls.Add(SecTextBox)
+        SecTextBox.Font = New Font(SecTextBox.Font.FontFamily, SecTextFontSize)
+        SecTextBox.Size = SecTextBoxSize
+        SecTextBox.Location = New Point(size.Width / 2 - SecTextBox.Size.Width / 2, size.Height - SecTextBoxSize.Height)
+        If time > 0 Then
+            SecTextBox.Text = time.ToString()
+            SecTextBox.Enabled = False
+            SetTime(time)
+        End If
+        SecTextBox.Mask = "999" 'digits including empty spaces
     End Sub
+
+    'Show initial chart
+    Public Sub ShowInitChart(ByVal width As Integer, ByVal height As Integer)
+        chart.ChartAreas(0).AxisX.Maximum = width
+        chart.ChartAreas(0).AxisY.Maximum = height
+    End Sub
+
+    'Set time from the textbox 
+    Public Sub SetTimeFromBox()
+        If Integer.TryParse(SecTextBox.Text, TimeInSec) Then
+            chart.ChartAreas(0).AxisX.Maximum = TimeInSec
+        End If
+    End Sub
+    'Set Time
+    Public Sub SetTime(ByVal time As Integer)
+        TimeInSec = time
+        chart.ChartAreas(0).AxisX.Maximum = TimeInSec
+    End Sub
+
     'Update function
     Public Overrides Sub Update(ByVal newVal() As Integer)
         If Not newVal.Length = NumOfMics Then
@@ -93,6 +135,8 @@ End Class
 
 Public Class BarGraph
     Inherits CGraph
+    'private global vars
+    Dim Labels As List(Of Label)
 
     'constructor
     Public Sub New(ByVal loc As Point,
@@ -101,10 +145,27 @@ Public Class BarGraph
                    ByVal mode As Modes)
 
         MyBase.New(loc, size, parent, SeriesChartType.Column, mode)
+        Labels = New List(Of Label)
+
+
         For i As Integer = 0 To NumOfMics - 1
             Dim tag As Integer = (i + 1) * 2
             series(i).Points.AddXY(tag, 0)
+            Dim l As Label = New Label()
+            Labels.Add(l)
+            Panel.Controls.Add(l)
+            l.Name = "P" + tag.ToString()
+            l.AutoSize = True
+            l.Font = New Font(l.Font.FontFamily, 20)
+            l.ForeColor = System.Drawing.Color.Black
+            l.BackColor = System.Drawing.Color.White
+            l.Size = New Size(65, 20)
+            l.Location = New Point(50 * tag, 200)
+            l.Text = "P" + tag.ToString()
+            l.BringToFront()
+
         Next
+
     End Sub
 
     'Update function
